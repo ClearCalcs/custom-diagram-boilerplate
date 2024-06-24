b63ea2d80fea94a3e3ba2e0f295bf5939f0413f1
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-06-24 20:14:45 +1000

    Simplify parcel resolver

--- parcelResolver.js
+++ parcelResolver.js
@@ -6,12 +6,12 @@ const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default new Resolver({
     async resolve({ specifier }) {
-        if (specifier === "../../src/interactive/interface") {
+        if (
+            specifier === "../../src/interactive/interface" &&
+            process.env.EXAMPLE_PATH
+        ) {
             return {
-                filePath: path.join(
-                    __dirname,
-                    process.env.EXAMPLE_PATH || "src/interactive/interface.ts",
-                ),
+                filePath: path.join(__dirname, process.env.EXAMPLE_PATH),
             };
         }
 
